a8ff3cb213bfb6d83a16599dae63b132a9140aec
Tulitas <tulitas1@gmail.com>
2020-12-14 20:52:18 +0200

    new application MFD

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>zet</title>
+</head>
+<body>
+<?php
+echo "This is my first PHP project!";
+?><br>
+<a href="landingPages/patientsLandingPages.php">landing page</a><br>
+
+</body>
+</html>
---
+++ landingPages/patientsLandingPages.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <script src="/sources/script/patientsLandingPage.js"></script>
+    <link href="/sources/css/patientsLandingPage.css">
+
+</head>
+<body>
+hi
+</body>
+</html>
